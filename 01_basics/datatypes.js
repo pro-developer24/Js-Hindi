af2ce416ba167392ca94a415ead7c167a92cace2
pro-developer24 <148115876+pro-developer24@users.noreply.github.com>
2023-12-08 18:25:50 +0000

    variables and datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert (5+5) // we are using nodejs, not browser
+
+
+console.log("output is here");
+
+let name = "devil"      //caracter
+let age  = 24          //number
+let isLoggedIn = true  //boolean
+
+// null = standalone value
+// Undefined =
+// symbol = unique value
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 144552
+let accountEmail = "devil@ffhf.com"
+var accountPassword = "64646"
+accountCity = "nagpur"
+
+let accountState;//undefined
+
+// accountId = 2 // not allowed in js
+console.log(accountId);
+
+accountEmail = "devil@ffhf.com" 
+accountPassword = "64646"
+accountCity = "nagpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+/* prefer not to use var becz  of issue in block scope and functional scope */
