cf09e916f6c162d33f65b7410f80e7a0c515067c
pro-developer24 <148115876+pro-developer24@users.noreply.github.com>
2023-12-08 18:51:54 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,18 @@
+
+
+// let score = 55
+
+// console.log(typeof score);
+// let valueInNumber = Number (score)
+
+//"55"  =>55
+//"55"  => NaN    not a number
+// true = 1; false = 0
+
+let isLoggedIn =     "Devil"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+// 1 = true;  0= false
+// empty string "" = false
+// "devil"  = true
